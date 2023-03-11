d8a893d2df900bccd86d4c76e25f1f30ff312fed
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-03-11 20:32:42 -0300

    Cep

---
+++ listadDeTarefa.js
@@ -0,0 +1,72 @@
+
+(() => {
+    
+    const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value 
+     
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+    
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+    lista.appendChild(tarefa)
+    input.value = " "
+
+
+}
+
+    const novaTarefa = document.querySelector('[data-form-button]')
+
+    novaTarefa.addEventListener('click', criarTarefa)
+
+    const BotaoConclui = () => {
+         const botaoConclui = document.createElement('button')
+    
+         botaoConclui.classList.add('check-button')
+         botaoConclui.innerText = 'concluir'
+
+        botaoConclui.addEventListener('click', concluirTarefa )
+
+    return botaoConclui
+
+
+}
+
+
+const concluirTarefa = (evento) => {
+     const botaoConclui = evento.target
+
+     const tarefaCompleta = botaoConclui.parentElement
+
+     tarefaCompleta.classList.toggle('done')
+}
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button')
+
+    botaoDeleta.innerText = 'deletar'
+    botaoDeleta.addEventListener('click', deletaTarefa)
+   
+    return botaoDeleta
+}
+
+const deletaTarefa = (evento) => {
+    const botaoDeleta = evento.target
+    
+    const tarefaCompleta = botaoDeleta.parentElement
+
+    tarefaCompleta.remove()
+
+    return botaoDeleta
+
+}
+
+})()
